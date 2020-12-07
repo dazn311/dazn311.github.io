90dc559542e54615bb15c658492cca86691ed387
Alexander <57184332+dazn311@users.noreply.github.com>
2020-12-07 14:06:31 +0300

    Initial commit2

---
+++ precache-manifest.1e4ba26f10c78dc93a2df2f8271386e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d61cb13e39f6c60d6bda628073b8a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4271ac8383d83fe4274",
+    "url": "/static/css/main.13876029.chunk.css"
+  },
+  {
+    "revision": "6b3acd338969eb93ca45",
+    "url": "/static/js/2.ff42ac10.chunk.js"
+  },
+  {
+    "revision": "f4271ac8383d83fe4274",
+    "url": "/static/js/main.b908eb46.chunk.js"
+  },
+  {
+    "revision": "0f7cb546645a38821887",
+    "url": "/static/js/runtime-main.b61f9a28.js"
+  }
+]);
