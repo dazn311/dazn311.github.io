1386c7a6cc4ad5398fc40e3c6068a8cf429c6023
Alexander <57184332+dazn311@users.noreply.github.com>
2020-12-10 17:57:05 +0300

    refactoring actions

--- src/store/actions/fav-actions.js
+++ src/store/actions/fav-actions.js
@@ -1,3 +1,9 @@
 export const ADD_TO_FAVORITE = 'ADD_TO_FAVORITE';
 export const REMOVE_TO_FAVORITE = 'REMOVE_TO_FAVORITE';
 export const REMOVE_ALL_TO_FAVORITE = 'REMOVE_ALL_TO_FAVORITE';
+
+export const addFavorite = item => { dispatch({ type: ADD_TO_FAVORITE, item }); };
+export const removeFavorite = id => dispatch({ type: REMOVE_TO_FAVORITE, id });
+export const removeFavoriteAll = id => dispatch({ type: REMOVE_ALL_TO_FAVORITE });
+export const getFavorite = () => { return state.favList || [] };
+export const setFavoriteToLocal = () => { localStorage.setItem('favoritesElements', JSON.stringify(state.favList)) };
