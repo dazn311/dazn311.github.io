28afa877452fe1190efe6307a2bda63a09ba7480
Alexander <57184332+dazn311@users.noreply.github.com>
2020-12-11 00:35:15 +0300

    add shift for favorite

--- src/store/reducers/productReducer.js
+++ src/store/reducers/productReducer.js
@@ -3,7 +3,7 @@ import { ADD_TO_FAVORITE, REMOVE_TO_FAVORITE, REMOVE_ALL_TO_FAVORITE, REMOVE_LAS
 const handlers = {
   [ADD_TO_FAVORITE]: (state, { item }) => ({ ...state, favList: [...state.favList, item] }),
   [REMOVE_TO_FAVORITE]: (state, { id }) => ({ ...state, favList: [...state.favList.filter(fav => fav.id !== id)] }),
-  [REMOVE_LAST_TO_FAVORITE]: (state) => ({ ...state, favList: [...state.favList.slice(0, -1)] }),
+  [REMOVE_LAST_TO_FAVORITE]: (state) => ({ ...state, favList: [...state.favList.shift()] }),
   [REMOVE_ALL_TO_FAVORITE]: (state) => ({ ...state, favList: [] }),
   DEFAULT: state => state
 }
